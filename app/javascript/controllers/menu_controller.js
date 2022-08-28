1a0dddd9e6a264604d89f1d9c516e17bb2318796
koki0317 <80045896+koki0317@users.noreply.github.com>
2022-08-28 11:58:29 +0900

    placed click me and added stimulus, changed hukidasi padding to %

--- app/javascript/controllers/menu_controller.js
+++ app/javascript/controllers/menu_controller.js
@@ -2,12 +2,12 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="menu"
 export default class extends Controller {
-  static targets = [ "hamburger"]
+  static targets = [ "hamburger", "clickme" ]
   connect() {
-    console.log("hello")
   }
 
   open() {
     this.hamburgerTarget.classList.toggle("menu-mobile")
-  }
+    this.clickmeTarget.classList.toggle("hidden")
+    }
 }
